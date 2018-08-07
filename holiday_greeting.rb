6c33db520e77c55c9b946952ad8e220995cf6d19
spethani <sarahpethani@gmail.com>
2018-08-07 16:59:34 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,3 +1,15 @@
 def holiday_greeting(to, from, holiday)
   "Happy #{holiday}, #{to}! From #{from}"
 end
+
+puts "Welcome!"
+puts "What is the holiday?"
+holiday=gets.chomp
+
+puts "Who is the card to?"
+to=gets.chomp
+
+puts "Who is the card from?"
+from=gets.chomp
+
+holiday_greeting(to, from, holiday)
